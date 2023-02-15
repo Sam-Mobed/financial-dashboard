8ba4b2253b90906494d451f9250655d19f0016c5
Sam-Mobed <masdebom@gmail.com>
2023-02-15 14:11:51 -0500

    finished the plot_data function, but im having issues with my python environments

--- main.py
+++ main.py
@@ -24,11 +24,37 @@ def load_data(ticker1, ticker2, start, end):
 
 #Plot the loaded data
 def plot_data(data, indicators, sync_axis=None):
-    pass
+    #we want to plot a candlestick chart with optional indicators
+    df = data
+    #there will be days with gains, and days with losses
+    gain = df.Close > df.Open
+    loss =  df.Open > df.Close
+    width_candlestick = 12 * 60 * 60 * 1000
+
+    if sync_axis is not None:
+        plot = figure(x_axis_type="datetime", tools="pan,wheel_zoom,box_zoom,reset,save", width=1000, x_range=sync_axis)
+    else:
+        plot = figure(x_axis_type="datetime", tools="pan,wheel_zoom,box_zoom,reset,save", width=1000)
+    
+    plot.xaxis.major_label_orientation = math.pi/4
+    plot.grid.grid_line_alpha = 0.25
+
+    #here we specify the starting point and the end point
+    plot.segment(df.index, df.High, df.index, df.Low, color="black")
+    #plot the actual candlestick,vertical for every gain and every loss
+    plot.vbar(df.index[gain], width_candlestick, df.Open[gain], df.Close[gain], fill_color= "#00ff00", line_color= "#00ff00")
+    plot.vbar(df.index[loss], width_candlestick, df.Open[loss], df.Close[loss], fill_color= "#ff0000", line_color= "#ff0000")
+
+    return plot
 
 #handle button clicks
 def on_button_click(ticker1, ticker2, start, end, indicators):
-    pass
+    df1, df2 = load_data(ticker1,ticker2,start,end)
+    plot1 = plot_data(df1, indicators)
+    plot2 = plot_data(df2, indicators, sync_axis=plot1.x_range)
+    curdoc.clear()
+    curdoc().add_root(layout)
+    curdoc().add_root(row(plot1,plot2))
 
 #Interface
 stock1_text = TextInput(title="Stock 1")
@@ -57,10 +83,5 @@ layout = column(stock1_text, stock1_text, date_picker_start, date_picker_end, in
 
 curdoc().clear()
 curdoc().add_root(layout)
-
-
-
-
-
 
 #if __name__==__main__
